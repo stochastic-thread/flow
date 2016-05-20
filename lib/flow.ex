b0e5ab7ee5f8e96ae4fae9ca52d96a38d76124a6
Arthur Colle <hello@arthurcolle.com>
2016-05-19 23:38:43 -0400

    Remove Agent that holds the socket

--- lib/flow.ex
+++ lib/flow.ex
@@ -5,14 +5,14 @@ defmodule Flow do
   def start_link do
     url =   "ws.pusherapp.com"
     path = "/app/de504dc5763aeef9ff52?client=js&version=3.0&protocol=5"
-    s = Socket.Web.connect!(url, path: path)
-    Agent.start_link(fn() -> s end, name: __MODULE__)
+    Socket.Web.connect!(url, path: path)
+    #
+    # Agent.start_link(fn() -> s end, name: __MODULE__)
   end
 
   defp get_socket_id(data) do
     data = JSX.decode!(data["data"])
     data["socket_id"]
-    IO.puts "colle"
   end
 
   defp create_payload(socket_id) do
@@ -38,24 +38,26 @@ defmodule Flow do
     Socket.Web.send!(socket, {:text, msg})
   end
 
-  def get_socket do
-    Agent.get(__MODULE__, fn x -> x end)
-  end
+  # def get_socket do
+  #   Agent.get(__MODULE__, fn x -> x end)
+  # end
 
-  def run do
-    Agent.get(__MODULE__, fn socket ->
-      case socket |> Socket.Web.recv! do
-        {:text, data} ->
-          assemble_payload(data)
-          |> send_msg(socket)
-          Flow.Listener.start_link(socket: socket)
-      end
-    end)
+  def run(socket) do
+    case socket |> Socket.Web.recv! do
+      {:text, data} ->
+        assemble_payload(data)
+        |> send_msg(socket)
+        Flow.Listener.start_link(socket: socket)
+    end
+
+    # Agent.get(__MODULE__, fn socket ->
+    #
+    # end)
   end
 
   def setup do
-    Flow.start_link
-    case Flow.run do
+    socket = Flow.start_link
+    case Flow.run(socket) do
       :ok -> Flow.get_socket
       _ -> IO.puts "thinking..."
     end
--- lib/flow/listener.ex
+++ lib/flow/listener.ex
@@ -1,7 +1,37 @@
 require IEx;
 
 defmodule Flow.Listener do
+  require Tirexs
   def start_link(opts), do: spawn(fn -> loop(opts[:socket]) end)
+
+  defp valid?(data) do
+    (data["bids"] != nil) and (data["asks"] != nil)
+  end
+
+  defp convert(price, quantity) do
+    [ String.to_float( price ), String.to_float( quantity ) ]
+  end
+
+  defp get_and_convert(data, side) do
+    type =
+      case side do
+        :bids -> "bids"
+        :asks -> "asks"
+      end
+    Enum.map(data[type], fn( [p, q] ) -> convert(p, q) end)
+  end
+
+  defp store_data(data, time) do
+    if valid?(data) do
+      bids =  get_and_convert(data, :bids)
+      asks =  get_and_convert(data, :asks)
+      url = "http://127.0.0.1:9200/bs/ob/"<>Integer.to_string(time)
+      json = JSX.encode!(%{:bids => bids, :asks => asks})
+      IO.puts("\n"<>url<>"\n")
+      IO.inspect Tirexs.HTTP.post!(url, json)
+      %{:bids => bids, :asks => asks}
+    end
+  end
 
   defp decode_response(txt) do
     case JSX.decode(txt) do
@@ -9,6 +39,7 @@ defmodule Flow.Listener do
         data
         |> Map.get("data")
         |> JSX.decode!
+        |> store_data(:os.system_time)
         |> IO.inspect
       {_else, idk} ->
         IO.puts(idk)
@@ -21,7 +52,8 @@ defmodule Flow.Listener do
         decode_response(txt)
         loop(s)
       {:ping, _ } ->
-          Flow.setup
+          Flow.run(s)
+      {_else, idk} -> IO.inspect idk
     end
   end
 end
