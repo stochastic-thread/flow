154ba792dff23553585cfe18d9b40457f51e20d0
Arthur Colle <hello@arthurcolle.com>
2016-05-21 16:14:06 -0400

    Transfer

--- lib/flow/utils.ex
+++ lib/flow/utils.ex
@@ -2,10 +2,25 @@ defmodule Flow.Utilities do
   def format_utc_timestamp(opts \\ [newline?: false]) do
     ts = {_, _, micro} = :os.timestamp()
     {{y, mo, d}, {h, mi, s}} = :calendar.now_to_local_time(ts)
-    str = :erlang.element(mo, {"Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug","Sep","Oct","Nov","Dec"})
-    str =
-      :io_lib.format("~2w_~s_~4w_~2w:~2..0w:~2..0w.~6..0w~n", [d, str, y, h, mi, s, micro])
-      |> to_string
+    months =
+      {
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      }
+    month_str = :erlang.element(mo, months)
+    data = [d, month_str, y, h, mi, s, micro]
+    fmt  = "~2w_~s_~4w_~2w:~2..0w:~2..0w.~6..0w~n"
+    str  = to_string( :io_lib.format(fmt, data) )
 
     case opts[:newline?] do
       true  -> str
