67d4cf50e72facd8bea0d2b02603864f338515a3
Arthur Colle <hello@arthurcolle.com>
2016-08-28 06:46:02 -0400

    Begin migration to genserver

--- lib/flow/listener.ex
+++ lib/flow/listener.ex
@@ -2,14 +2,54 @@ require IEx;
 
 defmodule Flow.Listener do
   require Tirexs
+  use GenServer
 
-  def start_link(opts) do
-    spawn(fn -> loop(opts[:socket]) end)
+  def start_link(options) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, options[:socket], name: __MODULE__)
+    # spawn(fn -> loop(opts[:socket]) end)
+    pid
   end
 
   defp valid?(data) do
     # checking existence of "bids" and "asks" keys in Map
     data["bids"] && data["asks"]
+  end
+
+  def send(pid, {:send, msg}), do: GenServer.call(pid, {:send, msg, socket})
+  def handle_call({:send, msg, socket}) do
+
+  end
+
+  def subscribe(pid, :subscribe), do: GenServer.call(pid, :subscribe)
+  def handle_call(:subscribe) do
+    url = Application.get_env(:flow, :service_url)
+    path = Application.get_env(:flow, :path)
+    socket = Socket.Web.connect!(url, path: path)
+
+    # receive initial subscription message
+    case Socket.Web.recv!( socket ) do
+      {:text, response_payload} ->
+        response_payload
+        |> assemble_payload
+        |> send_msg(socket)
+    end
+
+    {:reply, :subscribed, socket}
+  end
+
+  def listen(pid, {:listen, socket}), do: GenServer.cast(pid, {:listen, socket})
+  def handle_cast({:listen, s}) do
+    case Socket.Web.recv!(s) do
+      {:text, txt} ->
+        IO.inspect("Printing socket receive for (:text):\n#{txt}")
+        decode_response(txt)
+      {:ping, _ } ->
+        Socket.Web.send!(s, {:pong, ""})
+      {hey, idk} ->
+        IO.puts("Printing socket receive for (:#{hey})")
+        IO.puts "IDK????"
+        IO.inspect idk
+    end
   end
 
   defp convert(price, quantity) do
@@ -45,12 +85,13 @@ defmodule Flow.Listener do
       File.open(logfile, [:append], fn(file) ->
         IO.write(file, ts <> "\n")
       end)
-      json = JSX.encode!(%{:bids => bids, :asks => asks})
-      IO.puts("\n"<>url<>"\n")
+      package =
+        %{ :bids => bids,
+           :asks => asks }
+      json = JSX.encode!(package)
+      IO.puts("\n" <> url <> "\n")
       IO.inspect Tirexs.HTTP.post!(url, json)
-
-      %{ :bids => bids,
-         :asks => asks }
+      package
     end
   end
 
@@ -70,24 +111,6 @@ defmodule Flow.Listener do
       {:error, idk} ->
         IO.puts "~~ERROR~~"
         IO.inspect( idk )
-    end
-  end
-
-  defp loop(s) do
-    IO.inspect("Printing socket")
-    IO.inspect(s)
-    case Socket.Web.recv!(s) do
-      {:text, txt} ->
-        IO.inspect("Printing socket receive for (:text):\n#{txt}")
-        decode_response(txt)
-        loop(s)
-      {:ping, _ } ->
-        s |> Socket.Web.send!({:pong, ""})
-        loop(s)
-      {hey, idk} ->
-        IO.puts("Printing socket receive for (:#{hey})")
-        IO.puts "IDK????"
-        IO.inspect idk
     end
   end
 end
